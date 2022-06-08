ea1e39c6a1feb52bed46bdb111dbd2607051ee81
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-06-08 23:58:45 +0600

    feat: database setup done with environment variable and typeorm

--- src/data-source.ts
+++ src/data-source.ts
@@ -1,14 +1,15 @@
 import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
+import "dotenv/config"
 
 export const AppDataSource = new DataSource({
     type: "mysql",
     host: "localhost",
     port: 3306,
-    username: "test",
-    password: "test",
-    database: "test",
+    username: process.env.DB_USERNAME,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
     entities: [User],
