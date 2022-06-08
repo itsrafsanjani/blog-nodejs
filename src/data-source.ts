855d876ca900d182b9b15cb7c75268b17d58fc21
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-06-09 00:59:29 +0600

    feat: Photo model added and typeorm used, need to add crud

--- src/data-source.ts
+++ src/data-source.ts
@@ -2,6 +2,7 @@ import "reflect-metadata"
 import { DataSource } from "typeorm"
 import { User } from "./entity/User"
 import "dotenv/config"
+import { Photo } from "./entity/Photo";
 
 export const AppDataSource = new DataSource({
     type: "mysql",
@@ -12,7 +13,7 @@ export const AppDataSource = new DataSource({
     database: process.env.DB_NAME,
     synchronize: true,
     logging: false,
-    entities: [User],
+    entities: [User, Photo],
     migrations: [],
     subscribers: [],
 })
