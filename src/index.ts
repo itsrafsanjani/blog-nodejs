0fc2ae450c1df60cb38e39f5503a3b26db535e44
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-07-27 22:12:42 +0600

    feat: nodemon installed and used in npm start command

--- src/index.ts
+++ src/index.ts
@@ -7,19 +7,6 @@ app.use(express.json());
 const port = process.env.APP_PORT ?? 3000;
 
 AppDataSource.initialize().then(async () => {
-
-    console.log("Inserting a new user into the database...")
-    const user = new User()
-    user.firstName = "Timber"
-    user.lastName = "Saw"
-    user.age = 25
-    await AppDataSource.manager.save(user)
-    console.log("Saved a new user with id: " + user.id)
-
-    console.log("Loading users from the database...")
-    const users = await AppDataSource.manager.find(User)
-    console.log("Loaded users: ", users)
-
     // register routes
     app.get("/", function (req: Request, res: Response) {
       return res.send({
