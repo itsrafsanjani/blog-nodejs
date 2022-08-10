e65729637c056b09b43233eca21698cd6ed81d15
itsrafsanjani <mdrafsanjanirafin@gmail.com>
2022-08-11 00:26:59 +0600

    feat: register done :fire:

--- src/controller/AuthController.ts
+++ src/controller/AuthController.ts
@@ -3,24 +3,26 @@ import { Request, Response } from "express";
 import { AppDataSource } from "../data-source";
 import { User } from "../entity/User";
 import { BaseController } from "./BaseController";
-import { hash} from "bcryptjs";
+import { hash } from "bcryptjs";
 
 export class AuthController extends BaseController {
   register = async (req: Request, res: Response) => {
     const user = new User();
-
-    const errors = await validate(req.body, {
-      validationError: { target: false },
-    });
-    if (errors.length > 0) {
-      return this.responseWithError(res, "Validation error", errors, 422);
-    }
-
     const { email, firstName, lastName, age, password } = req.body;
     user.email = email;
     user.firstName = firstName;
     user.lastName = lastName;
     user.age = age;
+    user.password = password;
+
+    const errors = await validate(user, {
+      validationError: { target: false },
+    });
+
+    if (errors.length > 0) {
+      return this.responseWithError(res, "Validation error", errors, 422);
+    }
+
     user.password = await hash(password, 10);
 
     await AppDataSource.getRepository(User)
